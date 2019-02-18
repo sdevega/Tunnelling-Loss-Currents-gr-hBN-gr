9f3c70be88373396da1117810de4aaedbf5f52d9
sdevega <35807948+sdevega@users.noreply.github.com>
2019-02-18 22:34:48 +0100

    Add files via upload

---
+++ Jeh.cpp
@@ -0,0 +1,98 @@
+/*
+   Spectrally resolved tunnelling probability for a 
+   perfectly stacked gr-hBN-gr device.
+   ELIMINATING kp!! Final integral over Qi
+   Full-RPA for the conductivity.
+   T = 0K
+   Electron (e) doped graphene to hole (h) doped graphene.
+   Calculates Jeh(w) according to notes 15/feb/2019.
+   I1(kp,w) is tabulated and interpolated (NR3)
+   Combinations of Ef are: 
+      Ef2=0.3eV    Ef1=0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0eV
+      Ef2=0.5eV    Ef1=1.0eV
+    The vector to integrate over Qi is given by kpt
+   (profit from already being defined)
+*/ 
+#include "Jeh.h"
+
+int main(int argc, char **argv)
+{		
+	if(argc<4){printf("./jeh.x  Ef1(eV)  Ef2(eV)  Vb(eV)  \n\n"); exit(1);}
+	
+	int    i,j,l,m0;
+	 Ef1 = atof(argv[1])/eV;      // Fermi energy of layer 1
+	 Ef2 = atof(argv[2])/eV;      // Fermi energy of layer 2
+	 Vb  = atof(argv[3])/eV;	    // bias voltage
+ 	
+  double Jw_cv,Jw_vc,Jw_cc,Jw_vv,Jw;
+  char   ninw[150], nin[200];         // input files
+	char   nout[90];                    // output files
+
+  double Icv_tot,Ivc_tot,Ivv_tot,Icc_tot;
+
+	//--- Output for the spectral tunnelling probability
+  sprintf(nout,"Jeh_Ef1-%g_Ef2-%g_d1_Vb%g.dat",Ef1*eV,Ef2*eV,Vb*eV);  
+	FILE *fout=fopen(nout,"w");
+
+  // --- Tabulated frequencies     
+	sprintf(ninw,"../w2.dat");
+  //sprintf(ninw,"../2_EFdep_gr-hBN-gr_no-rotat/IntPhiW/w3.dat");
+	int     nw = norow(ninw);
+	FILE *finw = fopen(ninw,"r");
+	double *ww = new double[nw];
+	for(i=0;i<nw;++i) fscanf(finw,"%lf",ww+i);
+	fclose(finw);   
+  
+  // --- Angle varphi
+  for(l=0;l<nev;l++) phi[l]=var1+l*(var2-var1)/(nev-1.0);
+
+	for(l=0;l<nw;l++){
+		w  = ww[l]/eV;
+    g0 = (Ef1+Ef2-Vb+w)/vf; 
+    eta0 = (Ef1+Ef2-Vb)/vf;
+    
+    sprintf(nin,"../IntPhiWo/IntW_Ef1-%g_Ef2-%g_d1_w%g.dat",Ef1*eV,Ef2*eV,ww[l]);
+    //sprintf(nin,"../2_EFdep_gr-hBN-gr_no-rotat/IntPhiW/IntW_Ef1-%g_Ef2-%g_d1_w%g.dat",Ef1*eV,Ef2*eV,ww[l]);
+			FILE   *fin = fopen(nin,"r");
+			double  *kpf = new double[nk]; // kp from file
+			double  *I1f = new double[nk]; // I1 from file
+			for(i=0;i<nk;++i) fscanf(fin,"%lf %lf",kpf+i,I1f+i);
+			fclose(fin);
+
+			for(i=0;i<nk;i++){
+        kpt[i]=kpf[i]/nm;  // tabulated kp
+        I1t[i]=-I1f[i];		 // tabulated I1
+      }
+      delete [] kpf; kpf = NULL;
+      delete [] I1f; I1f = NULL;
+    
+    Linear_interp I1s(kpt,I1t); // interpolation constructor
+
+    // --- trapezoidal, Simpson and quadratures much slower
+    // ---    ->  rectangle sum (like trapezoidal)
+    Icv_tot=Ivc_tot=Ivv_tot=Icc_tot=0.0;
+    for(i=0;i<(nk-1);i++){
+      Icv_tot+=0.5*(Icv_Qi(kpt[i+1])+Icv_Qi(kpt[i]))*(kpt[i+1]-kpt[i]);
+      Ivc_tot+=0.5*(Ivc_Qi(kpt[i+1])+Ivc_Qi(kpt[i]))*(kpt[i+1]-kpt[i]);				
+      Ivv_tot+=0.5*(Ivv_Qi(kpt[i+1])+Ivv_Qi(kpt[i]))*(kpt[i+1]-kpt[i]);		
+      Icc_tot+=0.5*(Icc_Qi(kpt[i+1])+Icc_Qi(kpt[i]))*(kpt[i+1]-kpt[i]);		
+    }
+
+    Jw_cv = Icv_tot/pi/pi/pi/vf;  
+    Jw_vc = Icv_tot/pi/pi/pi/vf;  
+    Jw_vv = Icv_tot/pi/pi/pi/vf;  
+    Jw_cc = Icc_tot/pi/pi/pi/vf;  
+    Jw = Jw_cv+Jw_vc+Jw_vv+Jw_cc;
+	  fprintf(fout,"%g %g %g ",w*eV,Jw_cv*nm*nm,Jw_vc*nm*nm);
+    fprintf(fout,"%g %g %g \n",Jw_vv*nm*nm,Jw_cc*nm*nm,Jw*nm*nm);	
+    fflush(fout);
+    printf("-->  w(eV)=%g   J=%g\n",ww[l],Jw*nm*nm);
+    
+	} 
+	delete [] ww;  ww  = NULL;
+  delete [] phi; phi = NULL;
+
+  fclose(fout);
+
+  return 0;
+}
---
+++ Jhe.h
@@ -0,0 +1,62 @@
+/*
+   *** HEADER FOR Jhe.cpp ***
+	Spectrally resolved tunnelling probability for a 
+   perfectly stacked gr-hBN-gr device.
+   ELIMINATING kp!! Final integral over Qi
+   Full-RPA for the conductivity.
+   T = 0K
+   Hole (h) doped graphene to electron (e) doped graphene.
+   Calculates Jee(w) according to notes 15/feb/2019.
+   I1(kp,w) is tabulated and interpolated (NR3)
+   Combinations of Ef are: 
+      Ef2=0.3eV    Ef1=0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0eV
+      Ef2=0.5eV    Ef1=1.0eV
+	Notation: 
+		c: conduction
+		v: valence
+		Iif_x: integrand for integral over x that corresponds to the tunnel from
+		         i (c or v) to f (c or v)
+*/ 
+
+#include "J.h"
+
+//double err=1.0e-21;  int nmax=6000;   // integration parameters
+
+// --- Integral 1: cond-val
+double Ivc_vph(double vph){ // Integrand for  Int d_vph
+   double mm,pp;
+   double  I102m,I102p;
+   kp02m = kp002m(Qi,vph,g0);
+   kp02p = kp002p(Qi,vph,g0);
+   Qf02m = Qf00(Qi,vph,kp02m);
+   Qf02p = Qf00(Qi,vph,kp02p);
+	A2m   = A02m(Qi,vph,g0);
+   A2p   = A02p(Qi,vph,g0); 
+   En1   = vf*Qi - Ef1;
+   En2m  = vf*Qf02m - Ef2;
+   En2p  = vf*Qf02p - Ef2;
+
+   I102m = I1s.interp(kp02m);
+   if     (w<vf*(-2.0*Qi-kp02m-eta0)) mm=0.0;
+   else if(w>vf*(-2.0*Qi+kp02m-eta0)) mm=0.0;
+   else  mm=kp02m*I102m*A2m*FD(En1)*FD(En2m);
+
+   I102p = I1s.interp(kp02p);
+   if     (w<vf*(-2.0*Qi-kp02p-eta0)) pp=0.0;
+   else if(w>vf*(-2.0*Qi+kp02p-eta0)) pp=0.0;
+   else  pp=kp02p*I102p*A2p*FD(En1)*FD(En2p);
+
+   return mm+pp;
+}
+
+double Ivc_Qi(double Qi_){ // integrand for  Int d_Qi
+   Qi = Qi_;
+
+   sum = 0.0;
+   for(j=0;j<(nev-1);j++){
+      sum += 0.5*(Ivc_vph(phi[j+1])+Ivc_vph(phi[j]))*(phi[j+1]-phi[j]);
+   }
+
+   return Qi*sum;
+   //return Qi*Qi*qtrap(var1,var2,Icv_vph); // much slower
+}
